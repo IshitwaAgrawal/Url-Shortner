2d2779d2b296f201b8e6deb75b0b02f2f092ac66
IshitwaAgrawal <ishitwa2000@gmail.com>
2021-07-09 14:01:07 +0530

    protoype completed

--- src/components/CreatedUrl.js
+++ src/components/CreatedUrl.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 const CreatedUrl = () =>{
     return (
-        <div className="createdurl"></div>
+        <div className="createdurl">
+            <div className="createdurl__info">
+                <a className="createdurl__info__shortUrl" target="_blank" href="https://google.com">https://google.com</a>
+                <a className="createdurl__info__longUrl" target="_blank" href="https://google.com">https://google.com</a>
+            </div>
+            <h3 className="createdurl__clicks">10</h3>
+        </div>
     );
 };
 
--- src/components/CreatedUrls.js
+++ src/components/CreatedUrls.js
@@ -4,6 +4,7 @@ const CreatedUrls = () =>{
     return (
         <div className="createdUrls">
             <h3>Created Urls : 08</h3>
+            <h3>User's Count : 06</h3>
         </div>
     );
 };
