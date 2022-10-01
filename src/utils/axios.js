524494c16a595f585e55bb5dbe76cc564e3e99b8
IshitwaAgrawal <ishitwa2000@gmail.com>
2022-10-01 18:33:03 +0000

    modified docker config

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -5,7 +5,7 @@ const instance = axios.create({
   //baseURL: "http://localhost:6789",
   //baseURL: "https://url-shortner09.herokuapp.com/",
   //baseURL: "http://server2.ishitw.me:6789",
-  baseURL: "http://urlshort.ishitw.me:6789",
+  baseURL: "http://ishitw.me:6789",
 });
 
 export default instance;
